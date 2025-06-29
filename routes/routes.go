20972487a2ad28a5ff095506c1a1b371ca7c304f
Fakhruddin Hashmi <109304853+hashmi846003@users.noreply.github.com>
2025-06-29 12:50:48 +0530

    Update routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -1,14 +1,19 @@
+
 package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/hashmi846003/OAUTH-2.0-IMPLEMENTATION/controllers"
 )
 
 func Setup(app *fiber.App) {
 	api := app.Group("/user")
 
-	api.Get("/get-user", func(c *fiber.Ctx) error {
-		return c.SendString("Hello World!!")
-	})
+	api.Get("/get-user", controllers.User)
 
+	api.Post("/register", controllers.Register)
+
+	api.Post("/login", controllers.Login)
+
+	api.Post("/logout", controllers.Logout)
 }
